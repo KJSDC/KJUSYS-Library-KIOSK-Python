4d6fbb0c8e74c8816ab219f470e73d10b3a41cde
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-09-07 21:17:59 +0530

    Adapted code to the new hardware

---
+++ static/temp.js
@@ -0,0 +1,185 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startIDReadingBtn = document.getElementById("startIDReading");
+    const startBookReadingBtn = document.getElementById("startBookReading");
+    const stopKeystrokeBtn = document.getElementById("stopKeystroke");
+    const connectCOM = document.getElementById("connectCOM");
+    const disconnectCOM = document.getElementById("disconnectCOM");
+    const IDcard = document.getElementById("idCardData");
+    const Book = document.getElementById("bookData");
+  
+    const socket = io()
+  
+    ////////////////////////////
+    // FUNCTION DEFINITIONS
+    ////////////////////////////
+  
+    // Universal function to fetch endpoints
+    async function sendCommand(url) {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const result = await response.json();
+        console.log(result.status);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  
+    // Update book data function call
+    async function getBookData() {
+      try {
+        const response = await fetch("/getBookData");
+        const result = await response.json();
+        Book.value = result.book_data;      
+  
+      } catch (error) {
+        console.error("Error fetching read data:", error);
+      }
+    }
+  
+      // Update id data function call
+      async function getIDData() {
+        try {
+          const response = await fetch("/getIDData");
+          const result = await response.json();
+          IDcard.value = result.id_data;      
+    
+        } catch (error) {
+          console.error("Error fetching read data:", error);
+        }
+      }
+  
+    // Load the ports into both the drop down menus
+    function loadPorts() {
+      fetch('/ports')
+        .then(response => response.json())
+        .then(data => {
+          const portList = document.getElementById('portList');
+          portList.innerHTML = '';
+  
+          data.ports.forEach(port => {
+            const option = document.createElement('option');
+            option.value = port;
+            option.text = port;
+            portList.appendChild(option);
+          });
+  
+        })
+        .catch(error => {
+          console.error('Error fetching ports:', error);
+        });
+    }
+  
+    // Upon selecting the ports and pressing connect, send the serialports and start the listener
+    function connectSerialPort() {
+      const selectedPort = document.getElementById('portList').value;
+      fetch('/libraryPortConnect', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ selectedPort: selectedPort}),
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.success) {
+            // enable the buttons once the listener is established
+            startIDReadingBtn.disabled = false;
+            startBookReadingBtn.disabled = false;
+            stopKeystrokeBtn.disabled = false;
+  
+            connectCOM.classList.add("active");
+            connectCOM.textContent = "Connection Established";
+  
+            alert('Connected to ' + selectedPort);
+          } else {
+            alert('Failed to connect: ' + data.error);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  
+    ////////////////////////////////
+    // EVENT LISTENERS
+    ////////////////////////////////
+  
+    // Sending connectSerialPort function call on pressing connect
+    connectCOM.addEventListener("click", (event) => {
+      event.preventDefault();
+      connectSerialPort();
+      connectCOM.disabled = true;
+    });
+  
+    // Fetch endpoint to terminate both the threads that are running
+    disconnectCOM.addEventListener("click", (event) => {
+      event.preventDefault()
+      sendCommand("/breakConnection")
+      connectCOM.disabled = false;
+      connectCOM.classList.remove("active");
+      connectCOM.textContent = "Connect";
+    });
+  
+    // Fetch endpoint to start the ID thread
+    startIDReadingBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      sendCommand("/IDreadingStatus/1");
+  
+      startIDReadingBtn.disabled = true;
+      startIDReadingBtn.textContent = "Read Active";
+      startIDReadingBtn.classList.add("active");
+    });
+  
+    // Fetch endpoint to start the Book thread
+    startBookReadingBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      sendCommand("/BookreadingStatus/1");
+  
+      startBookReadingBtn.disabled = true;
+      startBookReadingBtn.textContent = "Read Active";
+      startBookReadingBtn.classList.add("active");
+    });
+  
+    // Fetch endpoint to stop the keystrokes thread
+    stopKeystrokeBtn.addEventListener("click", async (event) => {
+      event.preventDefault();
+      sendCommand("/IDreadingStatus/0");
+      // Delay to mitigate serial conflict
+      await new Promise((r) => setTimeout(r, 500));
+      sendCommand("/BookreadingStatus/0");
+  
+  
+      // Restore ID button
+      startIDReadingBtn.textContent = "Start ID Reading";
+      startIDReadingBtn.classList.remove("active");
+      startIDReadingBtn.disabled = false;
+  
+      // Restore Book button
+      startBookReadingBtn.textContent = "Start Book Reading";
+      startBookReadingBtn.classList.remove("active");
+      startBookReadingBtn.disabled = false;
+    });
+  
+    // Dynamic id data updation
+    socket.on('id_changed', function(data){
+      const updated_value = data.new_id;
+      IDcard.value = updated_value
+    })
+  
+    // Dynamic book data updation
+    socket.on('book_changed', function(data){
+      const updated_value = data.new_book;
+      Book.value = updated_value
+    })
+  
+    loadPorts();
+    // setInterval(getBookData, 500);
+    // setInterval(getIDData, 500);
+  
+  });
+  
---
+++ temp.py
@@ -0,0 +1,187 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit
+import serial
+import threading
+import serial.tools.list_ports
+# import pyautogui
+import time
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Global variables for serial ports and threads
+ser = None
+id_thread = None
+book_thread = None
+stop_threads = False
+
+# Global variables to store the latest data from each Arduino
+id_data = ""
+book_data = ""
+
+# Commands
+id_read_command = "I"
+book_read_command = "B"
+write_command = ";"
+stop_write_command = ":"
+
+# Function to send commands to serial
+def Serial_write(command):
+    if ser:
+        ser.write(command.encode())
+        print(f"Command sent to serial: {command}")
+
+# Making it so that instead of endpoint calls every 0.5 seconds, it updates id data dynamically using sockets
+def notify_id_change(new_id):
+    global id_data
+    id_data = new_id
+    socketio.emit('id_changed', {'new_id': new_id})
+
+# Making it so that instead of endpoint calls every 0.5 seconds, it updates book data dynamically using sockets
+def notify_book_change(new_book):
+    global book_data
+    book_data = new_book
+    socketio.emit('book_changed', {'new_book': new_book})
+
+# Function to send command to serial to start the id read for 3 seconds
+# once the read starts, read the next line data, ie, the id data and update it on the web
+def get_id_read_data():
+    global stop_threads, id_data
+    while not stop_threads:
+        try:
+            if ser.in_waiting > 0:
+                id_data = ser.readline().strip().decode('utf-8')
+
+            if id_data == "PCD_Authenticate() failed: Error in communication." or id_data == "PCD_Authenticate() failed: Timeout in communication." or id_data == "MIFARE_Read() failed: The CRC_A does not match." :
+                    print(f"Encountered error: {id_data}")
+            else:
+                notify_id_change(id_data)
+                print(f"ID data updated, ID: {id_data}")
+        
+        except serial.SerialException as e:
+            print(f"Error reading the data from serial: {e}")
+            break
+
+# Function to send command to serial to start the id read for 3 seconds
+# once the read starts, read the next line data, ie, the id data and update it on the web
+def get_book_read_data():
+    global stop_threads, book_data
+    while not stop_threads:
+        try:
+            if ser.in_waiting > 0:
+                book_data = ser.readline().strip().decode('utf-8')
+
+            if book_data == "PCD_Authenticate() failed: Error in communication." or id_data == "PCD_Authenticate() failed: Timeout in communication." or id_data == "MIFARE_Read() failed: The CRC_A does not match." :
+                    print(f"Encountered error: {book_data}")
+            else:
+                notify_book_change(book_data)
+                print(f"ID data updated, ID: {book_data}")
+        
+        except serial.SerialException as e:
+            print(f"Error reading the data from serial: {e}")
+            break
+
+# Endpoint definitions
+
+# Main page
+@app.route('/')
+def index():
+    return render_template('library.html')
+
+# List ports
+@app.route('/ports', methods=['GET'])
+def get_ports():
+    ports = [port.device for port in serial.tools.list_ports.comports()]
+    return jsonify({'ports': ports})
+
+# Connect to the selected port
+@app.route('/libraryPortConnect', methods=['POST'])
+def connect_ports():
+    global ser, id_thread, book_thread, stop_threads
+    data = request.json
+
+    port = data.get('selectedPort')
+
+    try:
+        # Establish connections to both serial ports
+        ser = serial.Serial(port, baudrate=9600, timeout=1)
+        # stop_threads = False
+
+        return jsonify({'success': True})
+    except serial.SerialException as e:
+        return jsonify({'success': False, 'error': str(e)})
+    
+@app.route('/IDreadingStatus/<int:state>', methods=['POST'])
+def id_status(state):
+    global stop_threads, id_thread
+
+    if state == 1:
+        stop_threads = False
+        id_thread = threading.Thread(target=get_id_read_data)
+        id_thread.start()
+        print("ID reader thread started...")
+
+        while not stop_threads:
+            Serial_write(id_read_command)
+            time.sleep(3)
+    
+    elif state == 0:
+        stop_threads = True
+        if id_thread is not None:
+            id_thread.join()
+            print("ID reader thread stopped...")
+        return jsonify({'status': 'ID read thread terminated'})
+    
+    else:
+        return jsonify({'status': "invalid state"})
+    
+@app.route('/BookreadingStatus/<int:state>', methods=['POST'])
+def book_status(state):
+    global stop_threads, book_thread
+
+    if state == 1:
+        stop_threads = False
+        book_thread = threading.Thread(target=get_book_read_data)
+        book_thread.start()
+        print("Book reader thread started...")
+
+        while not stop_threads:
+            Serial_write(book_read_command)
+            time.sleep(3)
+    
+    elif state == 0:
+        stop_threads = True
+        if book_thread is not None:
+            book_thread.join()
+            print("ID reader thread stopped...")
+        return jsonify({'status': 'Book read thread terminated'})
+    
+    else:
+        return jsonify({'status': "invalid state"})
+
+@app.route('/breakConnection', methods=['POST'])
+def break_connection():
+    global ser, stop_threads
+
+    stop_threads = True
+    if ser.is_open:
+        ser.close()
+
+    return jsonify({'status': 'Connections terminated'})
+
+@app.route('/getBookData', methods=['GET'])
+def get_book_data():
+    # Return the latest book data from PN5180 reader
+    return jsonify({
+        'book_data': book_data
+    })
+
+@app.route('/getIDData', methods=['GET'])
+def  get_id_data():
+    # Return the latest id data from mfrc reader
+    return jsonify({
+        'id_data': id_data
+    })
+
+if __name__ == '__main__':
+    socketio.run(app)
